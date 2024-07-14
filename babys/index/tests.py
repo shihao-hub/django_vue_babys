6ae564c4c876a8737247a5701bbfe74af5c74d38
shihao-hub <2958017271@qq.com>
2024-07-14 19:20:34 +0800

    general commit

--- babys/index/tests.py
+++ babys/index/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+class IndexTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_main(self):
+        pass
